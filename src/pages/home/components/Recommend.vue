44966fbc460d2cfe6891788752bb043dc7867d8a
sugarbeans <a181800922@gmail.com>
2018-08-03 01:01:59 +0800

    recommend and weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li v-for="(item,index) of recommendList" :key="index" class="item border-bottom">
+        <img :src="item.imgUrl" alt="" class="item-img" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeRecommend",
+    data: function () {
+      return {
+        recommendList: [
+          {
+            id: '1001',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/64/34362835b4a219af8b36803d31a6380f.water.jpg_200x200_bc5c0452.jpg',
+            title: '深圳小梅沙海洋世界',
+            desc: '浪漫的海洋世界,浪漫之旅,小梅沙等你来'
+          },
+          {
+            id: '1002',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/64/34362835b4a219af8b36803d31a6380f.water.jpg_200x200_bc5c0452.jpg',
+            title: '深圳小梅沙海洋世界',
+            desc: '浪漫的海洋世界,浪漫之旅,小梅沙等你来'
+          },
+          {
+            id: '1003',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/64/34362835b4a219af8b36803d31a6380f.water.jpg_200x200_bc5c0452.jpg',
+            title: '深圳小梅沙海洋世界',
+            desc: '浪漫的海洋世界,浪漫之旅,小梅沙等你来'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    display: flex
+    overflow: hidden
+    height: 2rem;
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .15rem
+    .item-info
+      flex: 1
+      padding: .15rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .30rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: .12rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of list"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data: function () {
+    return {
+      list: [
+        {
+          id: 1001,
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1602/88/bf120edeaea383.jpg_r_640x214_f8591f7b.jpg',
+          title: "欢乐长隆",
+          desc: "世界欢乐汇聚长隆"
+        },
+        {
+          id: 1002,
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1602/88/bf120edeaea383.jpg_r_640x214_f8591f7b.jpg',
+          title: "欢乐长隆",
+          desc: "世界欢乐汇聚长隆"
+        },
+        {
+          id: 1003,
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1602/88/bf120edeaea383.jpg_r_640x214_f8591f7b.jpg',
+          title: "欢乐长隆",
+          desc: "世界欢乐汇聚长隆"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.09%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
