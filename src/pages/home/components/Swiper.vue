529d9cd7dcc440f551b459d8b28ba31842a8b52c
sugarbeans <a181800922@gmail.com>
2018-08-02 18:53:10 +0800

    update localhost image

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -15,6 +15,11 @@
 </template>
 
 <script>
+  import swiper1 from '@/assets/images/swiper_01.jpg'
+  import swiper2 from '@/assets/images/swiper_02.jpg'
+  import swiper3 from '@/assets/images/swiper_03.jpg'
+  import swiper4 from '@/assets/images/swiper_04.jpg'
+  import swiper5 from '@/assets/images/swiper_05.jpg'
   export default {
     name: "HomeSwiper",
     data: function () {
@@ -24,10 +29,16 @@
           loop: true,
         },
         swiperList: [
-            {id: 1001, imgUrl: "http://img1.qunarzz.com/piao/fusion/1807/e8/ad9398da40eb6402.jpg_750x200_f9e744db.jpg"},
-            {id: 1002, imgUrl: "http://img1.qunarzz.com/piao/fusion/1806/3c/c72a1ccd4d7b2202.jpg_750x200_b88bbab4.jpg"},
-            {id: 1003, imgUrl: "http://img1.qunarzz.com/piao/fusion/1805/e8/14b75b1c81fbe702.jpg_750x200_e6d4f1f1.jpg"}
-          ]
+          /*{id: 1001, imgUrl: "http://img1.qunarzz.com/piao/fusion/1807/e8/ad9398da40eb6402.jpg_750x200_f9e744db.jpg"},
+          {id: 1002, imgUrl: "http://img1.qunarzz.com/piao/fusion/1806/3c/c72a1ccd4d7b2202.jpg_750x200_b88bbab4.jpg"},
+          {id: 1003, imgUrl: "http://img1.qunarzz.com/piao/fusion/1805/e8/14b75b1c81fbe702.jpg_750x200_e6d4f1f1.jpg"},
+          */
+          {id: 1001, imgUrl: swiper1},
+          {id: 1002, imgUrl: swiper2},
+          {id: 1003, imgUrl: swiper3},
+          {id: 1004, imgUrl: swiper4},
+          {id: 1005, imgUrl: swiper5}
+        ]
       }
     }
   }
