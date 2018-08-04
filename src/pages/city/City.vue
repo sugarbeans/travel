8e7cec625134a4e88bfc0dc7aa9c10c15768e551
sugarbeans <a181800922@gmail.com>
2018-08-04 22:22:46 +0800

    error of search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :cities="cities" :hotCities="hotCities" :letter="keyChange"></city-list>
     <city-alphabet @change="getKey" :cities="cities"></city-alphabet>
   </div>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,48 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名和拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名和拼音">
+    </div>
+    <div class="search-content">
+      <ul>
+        <li v-for="item of list">{{item.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "CitySearch"
+    name: "CitySearch",
+    props: {
+      cities: Object
+    },
+    data: function () {
+      return {
+        keyword: '',
+        list: [],
+        timer: null
+      }
+    },
+    watch: {
+      keyword: function () {
+        if(this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(()=>{
+          const result = [];
+          for(let i in this.cities) {
+            this.cities[i].foreach((value)=>{
+              if (value.spell.indexof(this.keyword)>-1 ||
+              value.name.indexOf(this.keyword)>-1 ){
+                result.push(value);
+              }
+            })
+          }
+          this.list = result;
+        },100)
+      }
+    }
   }
 </script>
 
@@ -24,5 +60,18 @@
       width: 100%
       text-align: center
       border-radius: .06rem
+      color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    right: 0
+    left: 0
+    bottom: 0
+    background: green
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
       color: #666
 </style>
