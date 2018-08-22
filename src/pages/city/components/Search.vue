80dc132f50360432b71ca7af8e8d37185227e28a
sugarbeans <a181800922@gmail.com>
2018-08-22 09:56:53 +0800

    search logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,15 +3,17 @@
     <div class="search">
       <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名和拼音">
     </div>
-    <div class="search-content">
+    <div class="search-content" ref="search" v-show="keyword">
       <ul>
-        <li v-for="item of list">{{item.name}}</li>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNullData">没有找到匹配的数据</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+  import Bscroll from 'better-scroll'
   export default {
     name: "CitySearch",
     props: {
@@ -24,16 +26,25 @@
         timer: null
       }
     },
+    computed: {
+      hasNullData: function () {
+        return !this.list.length
+      }
+    },
     watch: {
       keyword: function () {
         if(this.timer) {
           clearTimeout(this.timer)
         }
+        if(!this.keyword){
+          this.list = [];
+          return
+        }
         this.timer = setTimeout(()=>{
           const result = [];
           for(let i in this.cities) {
-            this.cities[i].foreach((value)=>{
-              if (value.spell.indexof(this.keyword)>-1 ||
+            this.cities[i].forEach((value)=>{
+              if (value.spell.indexOf(this.keyword)>-1 ||
               value.name.indexOf(this.keyword)>-1 ){
                 result.push(value);
               }
@@ -42,6 +53,9 @@
           this.list = result;
         },100)
       }
+    },
+    mounted: function () {
+      this.scroll = new Bscroll(this.$refs.search);
     }
   }
 </script>
@@ -69,7 +83,7 @@
     right: 0
     left: 0
     bottom: 0
-    background: green
+    background: #eee
     .search-item
       line-height: .62rem
       padding-left: .2rem
